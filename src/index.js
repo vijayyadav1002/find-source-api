fcf13149c53e4aec19390000a9048f8c81a8fde5
Vijay Yadav <vijayyadav1002@gmail.com>
2018-01-01 16:38:54 -0600

    testing MONGODB_URI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
-
+console.log('MONGODB_URI', process.env.MONGODB_URI);
 app.listen(PORT, (err) => {
     if (err) {
         console.error('Whoops!! something went wrong', err);
